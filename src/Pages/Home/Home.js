e7f2cb80e67c9b562e478420a31330394c731810
Md. Masud Rana <mr.hasib45@gmail.com>
2022-08-28 22:17:40 +0600

    Made navbar

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner';
+import Info from './Info';
+
+const Home = () => {
+    return (
+        <div className=''>
+            <Banner />
+            <Info />
+        </div>
+    );
+};
+
+export default Home;

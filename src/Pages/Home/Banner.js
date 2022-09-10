4c65a91be357ce71f60118074446d76e0c593041
Md. Masud Rana <mr.hasib45@gmail.com>
2022-09-11 00:28:16 +0600

    solve some responsive issues

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,32 +5,28 @@ import bgImage from "../../assets/images/bg.png";
 
 const Banner = () => {
   return (
-    <div>
-      <div
-        class="hero min-h-screen"
-        style={{
-          background: ` url(${bgImage})`,
-          backgroundSize: "cover",
-          justifyContent: "center",
-        }}
-      >
-        <div class="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src={Chair}
-            class="max-w-lg rounded-lg shadow-2xl ml-10"
-            alt="chair"
-          />
-          <div>
-            <h1 class="text-5xl font-bold uppercase  ">
-              Your New Smile Starts Here
-            </h1>
-            <p class="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <PrimaryButton>{"Get Started"}</PrimaryButton>
-          </div>
+    <div
+      class="hero min-h-screen"
+      style={{
+        background: ` url(${bgImage})`,
+      }}
+    >
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src={Chair}
+          class="max-w-lg rounded-lg shadow-2xl"
+          alt="chair"
+        />
+        <div>
+          <h1 class="text-5xl font-bold">
+            Your New Smile Starts Here
+          </h1>
+          <p class="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <PrimaryButton>{"Get Started"}</PrimaryButton>
         </div>
       </div>
     </div>
@@ -38,3 +34,4 @@ const Banner = () => {
 };
 
 export default Banner;
+
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -2,75 +2,59 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const menuItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/appointment">Appointment</Link>
+      </li>
+      <li>
+        <Link to="/review">Review</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </>
+  );
   return (
-    <div>
-      <div class="navbar bg-base-100">
-        <div class="navbar-start">
-          <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabindex="0"
-              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <li>
-                <Link to="/home">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/appointment">Appointment</Link>
-              </li>
-              <li>
-                <Link to="/reviews">Reviews</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact Us</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-            </ul>
-          </div>
-          <Link to="/home" class="btn btn-ghost normal-case text-xl">Doctors Portal</Link>
-        </div>
-        <div class="navbar-center hidden lg:flex">
-          <ul class="menu menu-horizontal pl-20">
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/appointment">Appointment</Link>
-            </li>
-            <li>
-              <Link to="/reviews">Reviews</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact Us</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex="0"
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {menuItems}
           </ul>
         </div>
+        <a className="btn btn-ghost normal-case text-xl">Doctors Portal</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">{menuItems}</ul>
       </div>
     </div>
   );

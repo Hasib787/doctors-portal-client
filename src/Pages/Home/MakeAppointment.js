2797a05351124c37066404dd953468541653b6b3
Md. Masud Rana <mr.hasib45@gmail.com>
2022-09-18 00:27:50 +0600

    design MakeAppointment

---
+++ src/Pages/Home/MakeAppointment.js
@@ -0,0 +1,31 @@
+import React from "react";
+import doctor from "../../assets/images/doctor.png";
+import PrimaryButton from "../Shared/PrimaryButton";
+import appointment from "../../assets/images/appointment.png"
+
+const MakeAppointment = () => {
+  return (
+    <section style={{
+        background: `url(${appointment})`
+    }} className="flex justify-center items-center">
+      <div className="flex-1 hidden lg:block">
+        <img className="mt-[-125px]" src={doctor} alt="" />
+      </div>
+      <div className="flex-1">
+        <h3 className="text-xl text-primary font-bold">Appointment</h3>
+        <h2 className="text-3xl text-white">Make an appointment Today</h2>
+        <p className="text-white mb-5">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsumis that it has a more-or-less normal distribution of
+          letters,as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page
+        </p>
+        <PrimaryButton>Get Started</PrimaryButton>
+      </div>
+    </section>
+  );
+};
+
+export default MakeAppointment;

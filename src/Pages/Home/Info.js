2bd30cd58f6f16a2ab851b58faf90b513d8aa58b
Md. Masud Rana <mr.hasib45@gmail.com>
2022-09-18 00:27:07 +0600

    design info section

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import InfoCard from "./InfoCard";
+import clock from "../../assets/icons/clock.svg";
+import marker from "../../assets/icons/marker.svg";
+import phone from "../../assets/icons/phone.svg";
 
 const Info = () => {
-    return (
-        <div>
-            <h1>This is info </h1>
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 px-12">
+      <InfoCard cardTitle="Opening Hours" bgClass="bg-gradient-to-r to-primary from-secondary" img={clock} />
+      <InfoCard cardTitle="Visit our location" bgClass="bg-accent" img={marker} />
+      <InfoCard cardTitle="Contact us now" bgClass="bg-gradient-to-r to-primary from-secondary" img={phone} />
+    </div>
+  );
 };
 
 export default Info;

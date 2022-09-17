1df483f38cd368a9afd9fd9635d8b2db8569dd57
Md. Masud Rana <mr.hasib45@gmail.com>
2022-09-18 00:26:45 +0600

    some updates

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -6,7 +6,7 @@ import bgImage from "../../assets/images/bg.png";
 const Banner = () => {
   return (
     <div
-      class="hero min-h-screen"
+      className="hero min-h-screen px-12 "
       style={{
         background: ` url(${bgImage})`,
       }}
@@ -14,14 +14,14 @@ const Banner = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img
           src={Chair}
-          class="max-w-lg rounded-lg shadow-2xl"
+          className="max-w-lg rounded-lg shadow-2xl"
           alt="chair"
         />
         <div>
-          <h1 class="text-5xl font-bold">
+          <h1 className="text-5xl font-bold">
             Your New Smile Starts Here
           </h1>
-          <p class="py-6">
+          <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
+import Footer from './Footer';
 import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import Services from './Services';
+import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
         <div className=''>
             <Banner />
             <Info />
+            <Services />
+            <MakeAppointment />
+            <Testimonials />
+            <Contact />
+            <Footer />
         </div>
     );
 };
